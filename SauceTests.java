906935bd644610464fe6efc09e7603adbed9d1b0
sivanmir <109102123+sivanmir@users.noreply.github.com>
2022-08-15 00:26:00 +0300

    Update SauceTests.java

--- SauceTests.java
+++ SauceTests.java
@@ -2,6 +2,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SauceTests {
+
+    // Test #1 - add 2 products to the shppping cart and complete buying process :
     @Test
     public void loginTest() {
         Sauce sauce = new Sauce();
@@ -29,10 +31,12 @@ public class SauceTests {
         try {
             Thread.sleep(ms);
         } catch (InterruptedException e) {
-            //
+
         }
     }
 
+    // Test - Bug #1 - login to "standard_user" add products to the cart, then logout and login to
+    // "problem_user" and the added products will appear in the shopping cart of "problem_user":
     @Test
     public void cartleak() {
         Sauce sauce = new Sauce();
@@ -45,6 +49,8 @@ public class SauceTests {
 
     }
 
+
+    //Test - Bug #2 - the outer price doesn't match the inner price of the relevant product:
     @Test
     public void itemPrice() {
         Sauce sauce = new Sauce();
@@ -52,13 +58,15 @@ public class SauceTests {
         String OuterPrice = sauce.getText("#inventory_container > div > div:nth-child(1) > div.inventory_item_description > div.pricebar > div");
         sauce.click("#item_4_title_link > div");
         String InnerPrice = sauce.getText("#inventory_item_container > div > div > div.inventory_details_desc_container > div.inventory_details_price");
-        Assert.assertEquals(OuterPrice , InnerPrice);
+        Assert.assertEquals(OuterPrice, InnerPrice);
 
 
     }
 
+    //Test - Bug #3 -when adding a product by clicking on "add to cart" button then clicking on "remove" button
+    //but the "remove" button doesn't respond:
     @Test
-    public void RemoveFromCart (){
+    public void RemoveFromCart() {
         Sauce sauce = new Sauce();
         sauce.loginAs("problem_user", "secret_sauce");
         sauce.click("#add-to-cart-sauce-labs-backpack");
